7544bf7a98d4896a961b885684fb2342dceea496
EC2 Default User <ec2-user@ip-172-31-90-223.ec2.internal>
2021-12-01 06:45:32 +0000

    docker done

---
+++ pythonapp/cisco.py
@@ -0,0 +1,10 @@
+import time
+
+while True:
+    print("Hello all , welcome to python..!!")
+    time.sleep(3)
+    print("Welcome to Cisco India !..")
+    time.sleep(2)
+    print("Welcome to Containers by Docker engine..!!")
+    print("______________________")
+    time.sleep(3)
---
+++ script/data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while [ true ]
+do
+    echo  "Hello Cisco this is my data .."  >>/tmp/cisco.txt
+    sleep 10
+done
